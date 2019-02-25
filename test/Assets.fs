a3542b85cf4662ca6d27b3978830442758965c97
Dag Brattli <dag@brattli.net>
2019-02-25 19:52:17 +0100

    wip

--- test/Assets.fs
+++ test/Assets.fs
@@ -9,17 +9,46 @@ open Cognite.Sdk.Assets
 open Cognite.Sdk.Context
 
 
-let fetcher result (ctx: Context) =
-    async {
-        return result
+type Fetcher (response) =
+    let mutable _ctx: Context option = None
+
+    member this.Ctx =
+        _ctx
+
+    member this.Fetch (ctx: Context) = async {
+        _ctx <- Some ctx
+        return response
     }
 
 [<Tests>]
-let assetTests =
+let assetTests = testList "A test group" [
     testAsync "Get asset is Ok" {
         // Arrenge
         let response = File.ReadAllText("Assets.json")
-        let fetch = Ok response |> fetcher
+        let fetcher = Ok response |> Fetcher
+        let fetch = fetcher.Fetch
+
+        let ctx =
+            defaultContext
+            |> addHeader ("api-key", "test-key")
+            |> setFetch fetch
+
+        // Act
+        let! result = getAsset ctx 42L
+
+        // Assert
+        Expect.isOk result "Should be OK"
+        Expect.isSome fetcher.Ctx "Should be set"
+        Expect.equal fetcher.Ctx.Value.Method Get "Should be equal"
+        Expect.equal fetcher.Ctx.Value.Resource (Resource "/assets/42") "Should be equal"
+        Expect.equal fetcher.Ctx.Value.Query [] "Should be equal"
+    }
+
+    testAsync "Get assets is Ok" {
+        // Arrenge
+        let response = File.ReadAllText("Assets.json")
+        let fetcher = Ok response |> Fetcher
+        let fetch = fetcher.Fetch
 
         let ctx =
             defaultContext
@@ -31,4 +60,57 @@ let assetTests =
 
         // Assert
         Expect.isOk result "Should be OK"
+        Expect.isSome fetcher.Ctx "Should be set"
+        Expect.equal fetcher.Ctx.Value.Method Get "Should be equal"
+        Expect.equal fetcher.Ctx.Value.Resource (Resource "/assets") "Should be equal"
+        Expect.equal fetcher.Ctx.Value.Query [("name", "string")] "Should be equal"
     }
+
+    testAsync "Create asset empty is Ok" {
+        // Arrenge
+        let response = File.ReadAllText("Assets.json")
+        let fetcher = Ok response |> Fetcher
+        let fetch = fetcher.Fetch
+
+        let ctx =
+            defaultContext
+            |> addHeader ("api-key", "test-key")
+            |> setFetch fetch
+
+        // Act
+        let! result = createAsset ctx []
+
+        // Assert
+        Expect.isOk result "Should be OK"
+        Expect.isSome fetcher.Ctx "Should be set"
+        Expect.equal fetcher.Ctx.Value.Method Post "Should be equal"
+        Expect.equal fetcher.Ctx.Value.Resource (Resource "/assets") "Should be equal"
+        Expect.equal fetcher.Ctx.Value.Query [] "Should be equal"
+    }
+
+    testAsync "Create asset is Ok" {
+        // Arrenge
+        let response = File.ReadAllText("Assets.json")
+        let fetcher = Ok response |> Fetcher
+        let fetch = fetcher.Fetch
+
+        let ctx =
+            defaultContext
+            |> addHeader ("api-key", "test-key")
+            |> setFetch fetch
+
+        let asset: Asset = {
+
+        }
+
+        // Act
+        let! result = createAsset ctx []
+
+        // Assert
+        Expect.isOk result "Should be OK"
+        Expect.isSome fetcher.Ctx "Should be set"
+        Expect.equal fetcher.Ctx.Value.Method Post "Should be equal"
+        Expect.equal fetcher.Ctx.Value.Resource (Resource "/assets") "Should be equal"
+        Expect.equal fetcher.Ctx.Value.Query [] "Should be equal"
+    }
+]
