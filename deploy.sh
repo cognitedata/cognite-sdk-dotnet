11b7cc49129412b4cde08db120af287bb3732b4b
Dag Brattli <dag.brattli@cognite.com>
2019-05-20 13:14:25 +0200

    Use partial path

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 cp /nuget-credentials/nuget.config ./nuget.config
 TOKEN=`xmllint --xpath 'string(/configuration/apikeys/add/@value)' ./nuget.config`
 dotnet pack -c release
-dotnet nuget push src/bin/Release/CogniteSdk.*.nupkg -k $TOKEN -s https://cognite.jfrog.io/cognite/api/nuget/nuget-local
+dotnet nuget push src/bin/Release/ -k $TOKEN -s https://cognite.jfrog.io/cognite/api/nuget/nuget-local
