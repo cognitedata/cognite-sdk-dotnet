ba5cd2083f2e112e18d9ac938550552b68bc0ff0
Dag Brattli <dag.brattli@cognite.com>
2019-05-20 13:24:09 +0200

    Try with nuget config

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cp /nuget-credentials/nuget.config ./nuget.config
-TOKEN=`xmllint --xpath 'string(/configuration/apikeys/add/@value)' ./nuget.config`
+#TOKEN=`xmllint --xpath 'string(/configuration/apikeys/add/@value)' ./nuget.config`
 dotnet pack -c release
-dotnet nuget push src/bin/Release/ -k $TOKEN -s https://cognite.jfrog.io/cognite/api/nuget/nuget-local
+#dotnet nuget push src/bin/Release/ -s https://cognite.jfrog.io/cognite/api/nuget/nuget-local -k $TOKEN
+dotnet nuget push src/bin/Release/
