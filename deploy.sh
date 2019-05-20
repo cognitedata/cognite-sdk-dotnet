a7b7cab4356f94e88c99e9c9c2972e2f9f2afec1
Dag Brattli <dag.brattli@cognite.com>
2019-05-20 16:23:34 +0200

    Cleanup deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-#TOKEN=`xmllint --xpath 'string(/configuration/apikeys/add/@value)' ./nuget.config`
 dotnet pack -c release
-#dotnet nuget push src/bin/Release/ -s https://cognite.jfrog.io/cognite/api/nuget/nuget-local -k $TOKEN
 dotnet nuget push src/bin/Release/ -s https://cognite.jfrog.io/cognite/api/nuget/v3/nuget-local
