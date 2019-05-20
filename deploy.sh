c062923eb39dd9f6619f903e757fe9dc768e9bcf
Dag Brattli <dag.brattli@cognite.com>
2019-05-20 12:23:19 +0200

    Try with lower case nuget.config

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cp /jenkins-docker-builder/Nuget.Config .
+cp /jenkins-docker-builder/Nuget.Config ./.nuget.config
 dotnet pack -c release
 dotnet nuget push src/bin/Release/CogniteSdk.*.nupkg -s https://cognite.jfrog.io/cognite/api/nuget/nuget-local
