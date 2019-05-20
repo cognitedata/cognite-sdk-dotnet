9399360120db67abd70aed3cfc1cf3c6fa9e3cc7
Dag Brattli <dag.brattli@cognite.com>
2019-05-20 16:11:05 +0200

    Set source parameter for deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 #TOKEN=`xmllint --xpath 'string(/configuration/apikeys/add/@value)' ./nuget.config`
 dotnet pack -c release
 #dotnet nuget push src/bin/Release/ -s https://cognite.jfrog.io/cognite/api/nuget/nuget-local -k $TOKEN
-dotnet nuget push src/bin/Release/
+dotnet nuget push src/bin/Release/ -s https://cognite.jfrog.io/cognite/api/nuget/v3/nuget-local
