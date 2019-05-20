e1aac4e0ead4172d29efe5d17e2af45f4d73ffdc
Dag Brattli <dag.brattli@cognite.com>
2019-05-20 12:58:52 +0200

    Set token manually

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cp /jenkins-docker-builder/nuget.config ./nuget.config
-#TOKEN=`xmllint --xpath 'string(/configuration/apikeys/add/@value)' ./nuget.config`
+TOKEN=`xmllint --xpath 'string(/configuration/apikeys/add/@value)' ./nuget.config`
 dotnet pack -c release
-dotnet nuget push src/bin/Release/CogniteSdk.*.nupkg -s https://cognite.jfrog.io/cognite/api/nuget/nuget-local
+dotnet nuget push src/bin/Release/CogniteSdk.*.nupkg -k $TOKEN -s https://cognite.jfrog.io/cognite/api/nuget/nuget-local
