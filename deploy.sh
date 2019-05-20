142b7074d82c4a81ff8fa1abf9b7a8295e303680
Dag Brattli <dag.brattli@cognite.com>
2019-05-20 12:54:21 +0200

    Fix name of nuget.config file

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cp /jenkins-docker-builder/Nuget.Config ./.nuget.config
+cp /jenkins-docker-builder/nuget.config ./nuget.config
+#TOKEN=`xmllint --xpath 'string(/configuration/apikeys/add/@value)' ./nuget.config`
 dotnet pack -c release
 dotnet nuget push src/bin/Release/CogniteSdk.*.nupkg -s https://cognite.jfrog.io/cognite/api/nuget/nuget-local
