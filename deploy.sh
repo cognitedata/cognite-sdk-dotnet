d8a1d54d74432615b885bfb102233213f39df3e0
Dag Brattli <dag.brattli@cognite.com>
2019-07-27 08:20:47 +0200

    Consume protobuf as a NuGet to avoid project references.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mono /usr/local/bin/nuget.exe pack src/Fusion.NET.fsproj -IncludeReferencedProjects
-dotnet nuget push *.nupkg -s https://cognite.jfrog.io/cognite/api/nuget/nuget-local
+dotnet pack -c release
+dotnet nuget push src/bin/Release/*.nupkg -s https://cognite.jfrog.io/cognite/api/nuget/nuget-local
