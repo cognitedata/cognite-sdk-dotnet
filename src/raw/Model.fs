39df9c9484b467ded9600ea6bdb11d6557c3be28
ebrattli <eric@brattli.net>
2019-10-19 19:37:38 +0200

    Add list tables for database in raw

--- src/raw/Model.fs
+++ src/raw/Model.fs
@@ -5,8 +5,9 @@ namespace CogniteSdk.Raw
 
 open System.Collections.Generic
 open Oryx
+open CogniteSdk.Common
 
-/// Read/write raw database type.
+/// Read raw database type.
 type DatabaseEntity internal (name: string) =
     member val Name : string = name with get, set
 
@@ -29,6 +30,48 @@ type DatabaseItemsReadDto = {
     Items: DatabaseReadDto seq
     NextCursor : string option
 }
+
+/// Read raw database table type.
+type TableEntity internal (name: string) =
+    member val Name : string = name with get, set
+
+    new () = TableEntity(name=null)
+
+type TableReadDto = {
+    Name: string
+} with
+    member this.ToTableEntity () : TableEntity =
+        TableEntity(this.Name)
+
+
+[<CLIMutable>]
+type TableItems = {
+    Items: TableEntity seq
+    NextCursor: string
+}
+
+type TableItemsReadDto = {
+    Items: TableReadDto seq
+    NextCursor : string option
+}
+
+type DatabaseQuery =
+    private
+    | CaseLimit of int
+    | CaseCursor of string
+
+    /// Max number of results to return
+    static member Limit limit =
+        if limit > MaxLimitSize || limit < 1 then
+            failwith "Limit must be set to 1000 or less"
+        CaseLimit limit
+    /// Cursor return from previous request
+    static member Cursor cursor = CaseCursor cursor
+
+    static member Render (this: DatabaseQuery) =
+        match this with
+        | CaseLimit limit -> "limit", limit.ToString()
+        | CaseCursor cursor -> "cursor", cursor.ToString()
 
 type ClientExtension internal (context: HttpContext) =
     member internal __.Ctx =
