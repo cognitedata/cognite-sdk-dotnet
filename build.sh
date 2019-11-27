74a0fb1419126e809e60b2041a1ff04cb876b3cd
Dag Brattli <dag.brattli@cognite.com>
2019-11-27 13:52:09 +0100

    Fix build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-DIRS="src test/unit/fsharp test/unit/csharp test/integration playground/fsharp playground/csharp"
+DIRS="src test/unit/fsharp test/unit/csharp test/integration/fsharp test/integration/csharp playground/fsharp playground/csharp"
 for dir in $DIRS; do
 	rm -rf $dir/bin $dir/obj
 done
