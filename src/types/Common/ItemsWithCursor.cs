6037abfc99ee7a4b227c010412ae6a37810acbdf
Dag Brattli <dag.brattli@cognite.com>
2020-01-08 12:31:27 +0100

    Renamed class to match file

--- src/types/Common/ItemsWithCursor.cs
+++ src/types/Common/ItemsWithCursor.cs
@@ -3,11 +3,12 @@ using System.Text.Json.Serialization;
 
 namespace CogniteSdk.Types.Common
 {
-    public class Items<T>
+    public class ItemsWithCursor<T>
     {
         /// <summary>
         /// Items of type T.
         /// </summary>
+
         public IEnumerable<T> Items { get; set; }
         /// <summary>
         /// Cursor to next page of data items.
