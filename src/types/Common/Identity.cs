697489452a6460f2949f9f001f8fe1008ca34236
Dag Brattli <dag.brattli@cognite.com>
2020-01-09 22:22:52 +0100

    Add updated Oryx prerelease

--- src/types/Common/Identity.cs
+++ src/types/Common/Identity.cs
@@ -3,12 +3,12 @@ namespace CogniteSdk.Types.Common
     /// <summary>
     /// Abstract base for Identity case classes.
     /// </summary>
-    public abstract class Identity {}
+    public abstract class IdentityBase {}
 
     /// <summary>
     /// Id case class.
     /// </summary>
-    public class IdentityId : Identity
+    public class IdentityId : IdentityBase
     {
         /// <summary>
         /// A server-generated ID for the object.
@@ -19,8 +19,25 @@ namespace CogniteSdk.Types.Common
     /// <summary>
     /// ExternalId case class.
     /// </summary>
-    public class IdentityExternalId : Identity
+    public class IdentityExternalId : IdentityBase
     {
+        /// <summary>
+        /// The external ID provided by the client. Must be unique for the resource type.
+        /// </summary>
+        public string ExternalId { get; set; }
+    }
+
+    /// <summary>
+    /// Both Id and ExternalId case class. Needed for deserialization. Only one of them
+    /// will/should be set.
+    /// </summary>
+    public class Identity : IdentityBase
+    {
+        /// <summary>
+        /// A server-generated ID for the object.
+        /// </summary>
+        public long Id { get; set; }
+
         /// <summary>
         /// The external ID provided by the client. Must be unique for the resource type.
         /// </summary>
