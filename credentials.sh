20a4c2054200762d77dd9b4d286cfe88438125b4
Dag Brattli <dag.brattli@cognite.com>
2020-01-24 09:49:45 +0100

    Remove obsolete file

--- credentials.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-USERNAME=`xmllint --xpath "string(/configuration/packageSourceCredentials/CogniteV2/add[contains(@key,'Username')]/@value)" ./nuget.config`
-PASSWORD=`xmllint --xpath "string(/configuration/packageSourceCredentials/CogniteV2/add[contains(@key,'ClearTextPassword')]/@value)" ./nuget.config`
-
-mono .paket/paket.exe config add-credentials https://cognite.jfrog.io/cognite/api/nuget/nuget --username $USERNAME --password $PASSWORD
