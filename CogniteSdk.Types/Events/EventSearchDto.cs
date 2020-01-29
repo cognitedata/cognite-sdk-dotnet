fe66d00ebd1fe3997fc5392d562a9a05a5021b9c
ebrattli <eric@brattli.net>
2020-01-29 14:33:50 +0100

    Event search only on description

--- CogniteSdk.Types/Events/EventSearchDto.cs
+++ CogniteSdk.Types/Events/EventSearchDto.cs
@@ -6,7 +6,19 @@ using System.Collections.Generic;
 namespace CogniteSdk.Events
 {
     /// <summary>
+    /// Dto for description search
+    /// </summary>
+    public class DescriptionSearchDto
+    {
+        /// <summary>
+        /// Search on Description.
+        /// </summary>
+        /// <value></value>
+        public string Description { get; set; }
+    }
+
+    /// <summary>
     /// The event search DTO.
     /// </summary>
-    public class EventSearchDto : SearchQueryDto<EventFilterDto, SearchDto> { }
+    public class EventSearchDto : SearchQueryDto<EventFilterDto, DescriptionSearchDto> { }
 }
