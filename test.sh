36271d51fdfd5883671604ef96ec58ff6fc6ef1f
Dag Brattli <dag.brattli@cognite.com>
2020-02-03 17:44:09 +0100

    Fix code coverage

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 dotnet test CogniteSdk/test/fsharp /p:Exclude="[xunit*]*" /p:CollectCoverage=true /p:CoverletOutputFormat=json /p:CoverletOutput='coverage.json'
-dotnet test CogniteSdk/test/csharp /p:Exclude="[xunit*]*" /p:CollectCoverage=true /p:MergeWith='../csharp/coverage.json' /p:CoverletOutputFormat=lcov /p:CoverletOutput='../../../coverage.lcov'
+dotnet test CogniteSdk/test/csharp /p:Exclude="[xunit*]*" /p:CollectCoverage=true /p:MergeWith='../fsharp/coverage.json' /p:CoverletOutputFormat=lcov /p:CoverletOutput='../../../coverage.lcov'
 rm CogniteSdk/test/fsharp/coverage.json
