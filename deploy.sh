3aaf2e5aaf0df9bab7b6e830b182508978c6d853
Dag Brattli <dag.brattli@cognite.com>
2020-05-05 12:12:32 +0200

    Use Github actions instead of Travis (#203)
    * Use gitub actions instead of travis
    
    * Add paket restore targets
    
    * Replace build badge
    
    * Add environment variables for test stage

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-dotnet pack -c release -p:PackageVersion=$TRAVIS_TAG
-dotnet nuget push CogniteSdk.Types/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
-dotnet nuget push CogniteSdk/src/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
-dotnet nuget push Oryx.Cognite/src/bin/Release/*.nupkg -s $NUGET_SOURCE -k $NUGET_API_KEY
