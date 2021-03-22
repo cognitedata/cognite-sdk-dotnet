fe6566fa86d117b1570b62f9fcfe8baf5c6e1eca
Dag Brattli <dag.brattli@cognite.com>
2021-03-22 13:20:19 +0100

    Stable Oryx v3 (#241)
    * Stable Oryx v3
    
    - Fix logging by moving log operator last in chain
    - Update README.
    
    * Move log operator

--- Oryx.Cognite/src/Playground/Handler.fs
+++ Oryx.Cognite/src/Playground/Handler.fs
@@ -32,18 +32,18 @@ module Handler =
         >=> withResource url
         >=> withQuery parms
         >=> fetch
-        >=> log
         >=> withError decodeError
         >=> json jsonOptions
+        >=> log
 
     let post<'a, 'b> (content: 'a) (url: string) : IHttpHandler<unit, 'b> =
         POST
         >=> withResource url
         >=> withContent (fun () -> new JsonPushStreamContent<'a>(content, jsonOptions) :> _)
         >=> fetch
-        >=> log
         >=> withError decodeError
         >=> json jsonOptions
+        >=> log
 
     let postPlayground<'a, 'b> (content: 'a) (url: string) : IHttpHandler<unit, 'b> =
         withVersion Playground >=> post content url
@@ -57,9 +57,9 @@ module Handler =
         >=> withQuery parms
         >=> withContent (fun () -> new JsonPushStreamContent<'a>(content, jsonOptions) :> _)
         >=> fetch
-        >=> log
         >=> withError decodeError
         >=> json jsonOptions
+        >=> log
 
     let inline list (content: 'a) (url: string) : IHttpHandler<unit, 'b> =
         withCompletion HttpCompletionOption.ResponseHeadersRead
