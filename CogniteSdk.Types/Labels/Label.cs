069eeff5b3ae23db47bc5c1ebe1c1a2fd92bc0b9
Einar <einar.omang@cognite.com>
2021-10-05 13:35:41 +0200

    Labels support (#262)
    Support for creating, listing and deleting label definitions.

---
+++ CogniteSdk.Types/Labels/Label.cs
@@ -0,0 +1,34 @@
+﻿// Copyright 2021 Cognite AS
+// SPDX-License-Identifier: Apache-2.0
+
+namespace CogniteSdk
+{
+    /// <summary>
+    /// Label read class
+    /// </summary>
+    public class Label
+    {
+        /// <summary>
+        /// The external ID provided by the client. Must be unique for the resource type.
+        /// At most 255 characters.
+        /// </summary>
+        public string ExternalId { get; set; }
+        /// <summary>
+        /// Name of the label. Max 140 characters.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Optional description of the label. Max 500 characters.
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Optional internalid of DataSet this label belongs to.
+        /// </summary>
+        public long? DataSetId { get; set; }
+        /// <summary>
+        /// The number of milliseconds since 00:00:00 Thursday, 1 January 1970, Coordinated Universal Time (UTC),
+        /// minus leap seconds when this label was created.
+        /// </summary>
+        public long CreatedTime { get; set; }
+    }
+}
---
+++ CogniteSdk.Types/Labels/LabelDelete.cs
@@ -0,0 +1,12 @@
+﻿// Copyright 2021 Cognite AS
+// SPDX-License-Identifier: Apache-2.0
+
+namespace CogniteSdk
+{
+    /// <summary>
+    /// Delete a list of labels.
+    /// </summary>
+    public class LabelDelete : ItemsWithoutCursor<CogniteExternalId>
+    {
+    }
+}
