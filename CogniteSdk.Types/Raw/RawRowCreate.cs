7ba3be7519659284b699baf51f47e3bf1a84344c
Einar Omang <einar@omang.com>
2021-11-09 09:54:08 +0100

    Remove RawRowCreateJson properly

--- CogniteSdk.Types/Raw/RawRowCreate.cs
+++ CogniteSdk.Types/Raw/RawRowCreate.cs
@@ -1,14 +1,28 @@
 // Copyright 2020 Cognite AS
 // SPDX-License-Identifier: Apache-2.0
 
-using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+using CogniteSdk.Types.Common;
 
 namespace CogniteSdk
 {
     /// <summary>
     /// Class to write a row to a table in Raw.
     /// </summary>
-    public class RawRowCreate<T> : RawRowCreateJson<Dictionary<string, T>>
+    public class RawRowCreate<T>
     {
+        /// <summary>
+        /// Row key. Unique in table.
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Row data stored as type <typeparamref name="T"/>. This gets converted to JSON.
+        /// </summary>
+        public T Columns { get; set; }
+
+        /// <inheritdoc />
+        public override string ToString() => Stringable.ToString(this);
     }
 }
