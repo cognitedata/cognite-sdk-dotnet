dc08a6f11470805a28144a142de3d19b3e62deb2
Dag Brattli <dag.brattli@cognite.com>
2022-03-30 20:48:01 +0200

    F# SDK (#284)
    * Update Oryx
    
    * feat: add F# sdk
    
    * fix: rename and document merge of settings
    
    * docs: update README

---
+++ CogniteSdk.FSharp/src/DataPointAggregates.fs
@@ -0,0 +1,257 @@
+namespace CogniteSdk.FSharp
+
+open System
+open System.Text.RegularExpressions
+open Oryx.Cognite
+open Common
+
+
+module DataPointAggregateType =
+    type AggregateType =
+        | Average
+        | Count
+        | Sum
+        | Max
+        | Min
+        | Interpolation
+        | StepInterpolation
+        | DiscreteVariance
+        | ContinuousVariance
+        | TotalVariation
+
+        override x.ToString() =
+            match x with
+            | Average -> "average"
+            | Count -> "count"
+            | Sum -> "sum"
+            | Max -> "max"
+            | Min -> "min"
+            | Interpolation -> "interpolation"
+            | StepInterpolation -> "stepInterpolation"
+            | DiscreteVariance -> "discreteVariance"
+            | ContinuousVariance -> "continuousVariance"
+            | TotalVariation -> "totalVariation"
+
+    /// all known aggregate types
+    let knownAggregates =
+        [ Average
+          Count
+          Sum
+          Max
+          Min
+          Interpolation
+          StepInterpolation
+          DiscreteVariance
+          ContinuousVariance
+          TotalVariation ]
+
+open DataPointAggregateType
+
+type DataPointAggregatesQueryItem =
+    { Id: int64 option
+      ExternalId: string option
+      Start: string option
+      End: string option
+      Limit: int option
+      Aggregates: AggregateType list option
+      Granularity: string option
+      IncludeOutsidePoints: bool option }
+
+    member x.ToDataPointsQueryItem() =
+        let query = CogniteSdk.DataPointsQueryItem()
+
+        x.Id
+        |> Option.iter (fun internalId -> query.Id <- internalId)
+
+        x.ExternalId
+        |> Option.iter (fun externalId -> query.ExternalId <- externalId)
+
+        x.Start
+        |> Option.iter (fun start -> query.Start <- start)
+
+        x.End
+        |> Option.iter (fun end' -> query.End <- end')
+
+        x.Limit
+        |> Option.iter (fun limit -> query.Limit <- limit)
+
+        x.Aggregates
+        |> Option.iter (function
+            | [] -> ()
+            | agg -> query.Aggregates <- agg |> List.map (fun x -> x.ToString()))
+
+        x.Granularity
+        |> Option.iter (fun granularity -> query.Granularity <- granularity)
+
+        x.IncludeOutsidePoints
+        |> Option.iter (fun toInclude -> query.IncludeOutsidePoints <- toInclude)
+
+        query
+
+    static member Empty =
+        { Id = None
+          ExternalId = None
+          Start = None
+          End = None
+          Limit = None
+          Aggregates = None
+          Granularity = None
+          IncludeOutsidePoints = None }
+
+type DataPointAggregatesQuery =
+    { Start: string option
+      End: string option
+      Granularity: string option
+      Limit: int32 option
+      Aggregates: AggregateType list
+      IncludeOutsidePoints: bool option
+      Items: DataPointAggregatesQueryItem list }
+
+    member x.ToDataPointsQuery() =
+        let query = CogniteSdk.DataPointsQuery()
+
+        x.Start
+        |> Option.iter (fun start -> query.Start <- start)
+
+        x.End
+        |> Option.iter (fun end' -> query.End <- end')
+
+        x.Granularity
+        |> Option.iter (fun g -> query.Granularity <- g)
+
+        x.Limit
+        |> Option.iter (fun limit -> query.Limit <- limit)
+
+        query.Aggregates <-
+            match x.Aggregates with
+            | [] -> knownAggregates
+            | _ -> x.Aggregates
+            |> List.map (fun x -> x.ToString())
+
+        x.IncludeOutsidePoints
+        |> Option.iter (fun toInclude -> query.IncludeOutsidePoints <- toInclude)
+
+        query.Items <-
+            x.Items
+            |> List.map (fun queryItem -> queryItem.ToDataPointsQueryItem())
+
+        query
+
+    static member Empty =
+        { Items = []
+          Start = None
+          End = None
+          Limit = None
+          Aggregates = []
+          Granularity = None
+          IncludeOutsidePoints = None }
+
+module DataPointAggregates =
+    let list (query: DataPointAggregatesQuery) =
+        match query.Limit with
+        | _ -> query.ToDataPointsQuery() |> DataPoints.list
+
+    module Helpers =
+        open Com.Cognite.V1.Timeseries.Proto
+
+        let granularityToTimeSpan (strOpt: string option) : TimeSpan option =
+            /// Active pattern for parsing an integer.
+            let (|ParseInteger|_|) (str: string) =
+                match System.Int32.TryParse(str) with
+                | true, intvalue -> Some(intvalue)
+                | false, _ -> None
+
+            let (|ParseRegex|_|) regex str =
+                let m = Regex(regex).Match(str)
+
+                if m.Success then
+                    Some(List.tail [ for x in m.Groups -> x.Value ])
+                else
+                    None
+
+            let timeSpanDays = float >> TimeSpan.FromDays >> Some
+            let timeSpanHours = float >> TimeSpan.FromHours >> Some
+            let timeSpanMinutes = float >> TimeSpan.FromMinutes >> Some
+            let timeSpanSeconds = float >> TimeSpan.FromSeconds >> Some
+
+            strOpt
+            |> Option.bind (fun str ->
+                match str with
+                | ParseRegex "(\d{1,4})d" [ ParseInteger day ] -> timeSpanDays day
+                | ParseRegex "^d$" [] -> timeSpanDays 1
+                | ParseRegex "(\d{1,4})h" [ ParseInteger hour ] -> timeSpanHours hour
+                | ParseRegex "^h$" [] -> timeSpanHours 1
+                | ParseRegex "(\d{1,4})m" [ ParseInteger min ] -> timeSpanMinutes min
+                | ParseRegex "^m$" [] -> timeSpanMinutes 1
+                | ParseRegex "(\d{1,4})s" [ ParseInteger sec ] -> timeSpanSeconds sec
+                | ParseRegex "^s$" [] -> timeSpanSeconds 1
+                | _ -> None)
+
+        let getAggregateResultPerTimeseries (aggregateResponse: DataPointListResponse) =
+            aggregateResponse.Items
+            |> Seq.choose (fun timeseriesResult ->
+                match timeseriesResult.DatapointTypeCase with
+                | DataPointListItem.DatapointTypeOneofCase.AggregateDatapoints -> Some timeseriesResult
+                | _ -> None)
+
+        /// Convert response to list of datapointAggregates
+        /// returns a flattened sequence of all aggregate datapoints tupled with the corresponding timeseries id
+        let convertResponse (dataPointAggregateResponse: DataPointListResponse) (granularity: string option) =
+
+            let resultPerTimeseries =
+                dataPointAggregateResponse
+                |> getAggregateResultPerTimeseries
+
+            resultPerTimeseries
+            |> Seq.map (fun resultFromSingleTimeseries ->
+                let aggregatePoints = resultFromSingleTimeseries.AggregateDatapoints.Datapoints
+
+                aggregatePoints
+                |> Seq.map (fun aggregatePoint ->
+                    (resultFromSingleTimeseries.ExternalId,
+                     aggregatePoint,
+                     (granularity |> Option.defaultValue String.Empty))))
+
+            |> Seq.collect id
+
+        let getNextPointPerTimeseries
+            (endTime: int64 option)
+            (granularity: string option)
+            (dataPointAggregateResponse: DataPointListResponse)
+            =
+            let granularityAsTimeSpan =
+                granularityToTimeSpan granularity
+                |> Option.defaultValue (TimeSpan.FromDays(1.0))
+
+            dataPointAggregateResponse
+            |> getAggregateResultPerTimeseries
+            |> Seq.map
+                /// pick the last datapoint for each timeseries in the response
+                (fun dpAggList ->
+                    (dpAggList.AggregateDatapoints.Datapoints
+                     |> Seq.tryLast,
+                     dpAggList.ExternalId))
+            /// filter out exhausted timeseries (without last point)
+            |> Seq.choose (function
+                | Some dpAgg, extId ->
+                    Some(
+                        extId,
+                        (dpAgg.Timestamp |> fromCdfTimestamp)
+                            .Add(granularityAsTimeSpan)
+                            .ToUnixTimeMilliseconds()
+                    )
+                | _ -> None)
+            |> Seq.choose (fun (extId, nextTimestamp) ->
+                match extId, nextTimestamp, endTime with
+                | extId, nextTimestamp, Some endTime when nextTimestamp < endTime -> Some(extId, nextTimestamp)
+                | extId, nextTimestamp, None -> Some(extId, nextTimestamp)
+                | _ -> None)
+
+
+
+        let toDataPointsQueryItems (nextPoints: seq<string * int64>) =
+            nextPoints
+            |> Seq.map (fun (extId, timestamp) ->
+                { DataPointAggregatesQueryItem.Empty with
+                    Start = timestamp.ToString() |> Some
+                    ExternalId = Some extId })
---
+++ CogniteSdk.FSharp/src/Files.fs
@@ -0,0 +1,222 @@
+namespace CogniteSdk.FSharp
+
+open System
+open System.Collections.Generic
+
+open Oryx
+open Oryx.Cognite
+
+type FileFilter =
+    { Name: string option
+      DirectoryPrefix: string option
+      MimeType: string option
+      MetaData: Map<string, string>
+      AssetIds: int64 list
+      AssetExternalIds: string list
+      RootAssetIds: Identifier list
+      DataSetIds: Identifier list
+      AssetSubtreeIds: Identifier list
+      Source: string option
+      CreatedTime: TimeRange option
+      LastUpdatedTime: TimeRange option
+      UploadedTime: TimeRange option
+      SourceCreatedTime: TimeRange option
+      SourceModifiedTime: TimeRange option
+      ExternalIdPrefix: string option
+      Uploaded: bool option
+      // TODO: GeoLocation
+      Labels: LabelFilter option }
+
+    member x.ToFileFilter() =
+        let filter = CogniteSdk.FileFilter()
+
+        x.Name |> Option.iter (fun x -> filter.Name <- x)
+
+        x.DirectoryPrefix
+        |> Option.iter (fun x -> filter.DirectoryPrefix <- x)
+
+        x.ExternalIdPrefix
+        |> Option.iter (fun x -> filter.ExternalIdPrefix <- x)
+
+        x.MimeType
+        |> Option.iter (fun x -> filter.MimeType <- x)
+
+        x.Source
+        |> Option.iter (fun x -> filter.Source <- x)
+
+        x.Uploaded
+        |> Option.iter (fun x -> filter.Uploaded <- x)
+
+        if not (List.isEmpty x.AssetIds) then
+            filter.AssetIds <- x.AssetIds |> Array.ofList
+
+        if not (List.isEmpty x.RootAssetIds) then
+            filter.RootAssetIds <-
+                x.RootAssetIds
+                |> List.map (fun x -> x.ToIdentifier())
+                |> Array.ofList
+
+        if not (List.isEmpty x.AssetExternalIds) then
+            filter.AssetExternalIds <- x.AssetExternalIds |> Array.ofList
+
+        if not (List.isEmpty x.AssetSubtreeIds) then
+            filter.AssetSubtreeIds <-
+                x.AssetSubtreeIds
+                |> List.map (fun x -> x.ToIdentifier())
+                |> Array.ofList
+
+        if not (List.isEmpty x.DataSetIds) then
+            filter.DataSetIds <-
+                x.DataSetIds
+                |> List.map (fun x -> x.ToIdentifier())
+                |> Array.ofList
+
+
+        x.CreatedTime
+        |> Option.iter (fun x -> filter.CreatedTime <- x.ToTimeRange())
+
+        x.LastUpdatedTime
+        |> Option.iter (fun x -> filter.LastUpdatedTime <- x.ToTimeRange())
+
+        x.UploadedTime
+        |> Option.iter (fun x -> filter.UploadedTime <- x.ToTimeRange())
+
+        x.SourceCreatedTime
+        |> Option.iter (fun x -> filter.SourceCreatedTime <- x.ToTimeRange())
+
+        x.SourceModifiedTime
+        |> Option.iter (fun x -> filter.SourceModifiedTime <- x.ToTimeRange())
+
+        x.Labels
+        |> Option.iter (fun x -> filter.Labels <- x.ToLabelFilter())
+
+        if not (Map.isEmpty x.MetaData) then
+            filter.Metadata <- x.MetaData |> Dictionary
+
+        filter
+
+    /// Merge with other filter. Optional values are merged using `Option.orElse`.
+    /// Collections are merged to the union of the two collections.
+    member x.MergeWith(other: FileFilter option) : FileFilter =
+        match other with
+        | None -> x
+        | Some other ->
+            { Name = x.Name |> Option.orElse other.Name
+              MimeType = x.MimeType |> Option.orElse other.MimeType
+              DirectoryPrefix =
+                x.DirectoryPrefix
+                |> Option.orElse other.DirectoryPrefix
+              AssetIds = set (x.AssetIds @ other.AssetIds) |> List.ofSeq
+              RootAssetIds =
+                set (x.RootAssetIds @ other.RootAssetIds)
+                |> List.ofSeq
+              AssetExternalIds =
+                set (x.AssetExternalIds @ other.AssetExternalIds)
+                |> List.ofSeq
+              AssetSubtreeIds =
+                set (x.AssetSubtreeIds @ other.AssetSubtreeIds)
+                |> List.ofSeq
+              DataSetIds =
+                set (x.DataSetIds @ other.DataSetIds)
+                |> List.ofSeq
+              MetaData =
+                Map.toList x.MetaData @ Map.toList other.MetaData
+                |> Map.ofList
+              Source = x.Source |> Option.orElse other.Source
+              CreatedTime = x.CreatedTime |> Option.orElse other.CreatedTime
+              LastUpdatedTime =
+                x.LastUpdatedTime
+                |> Option.orElse other.LastUpdatedTime
+
+              SourceCreatedTime =
+                  x.SourceCreatedTime
+                  |> Option.orElse other.SourceCreatedTime
+              SourceModifiedTime =
+                x.SourceModifiedTime
+                |> Option.orElse other.SourceModifiedTime
+
+              UploadedTime = x.UploadedTime |> Option.orElse other.UploadedTime
+
+              Uploaded = x.Uploaded |> Option.orElse other.Uploaded
+              ExternalIdPrefix =
+                x.ExternalIdPrefix
+                |> Option.orElse other.ExternalIdPrefix
+              Labels = x.Labels |> Option.orElse other.Labels }
+
+    static member Empty: FileFilter =
+        { Name = None
+          DirectoryPrefix = None
+          MimeType = None
+          MetaData = Map.empty
+          AssetIds = []
+          AssetExternalIds = []
+          RootAssetIds = []
+          DataSetIds = []
+          AssetSubtreeIds = []
+          Source = None
+          CreatedTime = None
+          LastUpdatedTime = None
+          UploadedTime = None
+          SourceCreatedTime = None
+          SourceModifiedTime = None
+          ExternalIdPrefix = None
+          Uploaded = None
+          // TODO: GeoLocation
+          Labels = None }
+
+type FileQuery =
+    { Filter: FileFilter option
+      Limit: int32 option
+      Cursor: string option
+      Partition: string option }
+
+    member x.ToFileQuery() =
+        let query = CogniteSdk.FileQuery()
+
+        x.Filter
+        |> Option.map (fun x -> x.ToFileFilter())
+        |> Option.iter (fun x -> query.Filter <- x)
+
+        x.Limit |> Option.iter (fun x -> query.Limit <- x)
+
+        // TODO: fix when available in the SDK
+        // x.Partition
+        // |> Option.iter (fun x -> query.Partition <- x)
+
+        x.Cursor
+        |> Option.iter (fun x -> query.Cursor <- x)
+
+        query
+
+    static member Empty =
+        { Filter = None
+          Limit = None
+          Cursor = None
+          Partition = None }
+
+type FileSearch =
+    { Filter: FileFilter option
+      Limit: int option
+      Search: SearchName }
+
+    member x.ToFileSearch() =
+        let search = CogniteSdk.FileSearch()
+
+        x.Filter
+        |> Option.iter (fun x -> search.Filter <- x.ToFileFilter())
+
+        x.Limit
+        |> Option.iter (fun x -> search.Limit <- Nullable(x))
+
+        search.Search <- x.Search.ToSearch()
+
+        search
+
+
+module Files =
+    let list (query: FileQuery) (source: HttpHandler<unit>) =
+        match query.Limit with
+        | Some x when x = 0 -> Common.emptyQuery<CogniteSdk.File>
+        | _ ->
+            let query = query.ToFileQuery()
+            source |> Files.list query
