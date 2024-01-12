f5b2b2d0381be6a8cfedbd0a0834b8856a2ad2d8
Einar Omang <einar.omang@cognite.com>
2024-01-12 14:23:48 +0100

    Alpha APIs for time series status codes
    No tests yet, since these APIs only exist on azure-dev for the moment.

--- CogniteSdk/src/Resources/Alpha.cs
+++ CogniteSdk/src/Resources/Alpha.cs
@@ -22,6 +22,11 @@ namespace CogniteSdk.Resources
         public SimulatorsResource Simulators { get; }
 
         /// <summary>
+        /// Methods for alpha data points with status codes.
+        /// </summary>
+        public AlphaDataPointsResource DataPoints { get; }
+
+        /// <summary>
         /// Will only be instantiated by the client.
         /// </summary>
         /// <param name="authHandler">The authentication handler.</param>
@@ -29,6 +34,7 @@ namespace CogniteSdk.Resources
         internal AlphaResource(Func<CancellationToken, Task<string>> authHandler, FSharpFunc<IAsyncNext<HttpContext, Unit>, Task<Unit>> ctx) : base(authHandler, ctx)
         {
             Simulators = new SimulatorsResource(authHandler, ctx);
+            DataPoints = new AlphaDataPointsResource(authHandler, ctx);
         }
     }
 }
