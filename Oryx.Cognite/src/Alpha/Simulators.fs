9121c75617f3615d3473abe3bc31b777688bd986
abdullah <abdullah.ali@cognite.com>
2024-01-18 16:49:05 +0500

    Add console
    Co-authored-by: Ivan Polomani <ivan.polomanyi@cognite.com>

--- Oryx.Cognite/src/Alpha/Simulators.fs
+++ Oryx.Cognite/src/Alpha/Simulators.fs
@@ -10,6 +10,9 @@ open Oryx.Cognite.Alpha
 
 open CogniteSdk.Alpha
 
+open System
+open Oryx.SystemTextJson
+open Oryx.SystemTextJson.ResponseReader
 [<RequireQualifiedAccess>]
 module Simulators =
     open CogniteSdk
@@ -153,14 +156,34 @@ module Simulators =
         |> withAlphaHeader
         |> HttpHandler.update items modelRevisionsUrl
 
+    // let createSimulatorRoutines<'T when 'T :> ISimulatorRoutineCreate>
+    //     (items: SimulatorRoutineCreate<'T> seq)
+    //     (source: HttpHandler<unit>)
+    //     : HttpHandler<SimulatorRoutine seq> =
+    //     source
+    //     |> withLogMessage "simulators:createSimulatorRoutines"
+    //     |> withAlphaHeader
+    //     |> HttpHandler.create (items |> Seq.map (fun item -> item.Value)) routinesUrl
+
     let createSimulatorRoutines<'T when 'T :> ISimulatorRoutineCreate>
         (items: SimulatorRoutineCreate<'T> seq)
         (source: HttpHandler<unit>)
         : HttpHandler<SimulatorRoutine seq> =
-        source
-        |> withLogMessage "simulators:createSimulatorRoutines"
-        |> withAlphaHeader
-        |> HttpHandler.create items routinesUrl
+            source
+            |> withLogMessage "simulators:createSimulatorRoutines"
+            |> withAlphaHeader
+            |> HttpHandler.create
+                (items
+                |> Seq.map (fun item ->
+                    Console.WriteLine(item.Value |> json jsonOptions)
+                    item.Value
+                )
+                |> Seq.toArray // Convert the sequence to an array
+                |> fun itemsArray ->
+                    Console.WriteLine(itemsArray.Length)
+                    itemsArray
+                )
+                routinesUrl
 
     let list (query: SimulatorQuery) (source: HttpHandler<unit>) : HttpHandler<ItemsWithoutCursor<Simulator>> =
         source
