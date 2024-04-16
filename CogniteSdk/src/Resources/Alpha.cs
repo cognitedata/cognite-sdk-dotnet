c87106cb0033ee423247b159a11eec187a8bf3d7
Einar Omang <einar.omang@cognite.com>
2024-04-16 08:54:32 +0200

    Update datapoint protobufs, move to beta

--- CogniteSdk/src/Resources/Alpha.cs
+++ CogniteSdk/src/Resources/Alpha.cs
@@ -22,11 +22,6 @@ namespace CogniteSdk.Resources
         public SimulatorsResource Simulators { get; }
 
         /// <summary>
-        /// Methods for alpha data points with status codes.
-        /// </summary>
-        public AlphaDataPointsResource DataPoints { get; }
-
-        /// <summary>
         /// Will only be instantiated by the client.
         /// </summary>
         /// <param name="authHandler">The authentication handler.</param>
@@ -34,7 +29,6 @@ namespace CogniteSdk.Resources
         internal AlphaResource(Func<CancellationToken, Task<string>> authHandler, FSharpFunc<IAsyncNext<HttpContext, Unit>, Task<Unit>> ctx) : base(authHandler, ctx)
         {
             Simulators = new SimulatorsResource(authHandler, ctx);
-            DataPoints = new AlphaDataPointsResource(authHandler, ctx);
         }
     }
 }
