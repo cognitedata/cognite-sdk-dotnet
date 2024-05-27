e415da84fc334f70379f81bf07de00255071a472
Everton Colling <evertoncolling@gmail.com>
2024-05-27 17:40:29 +0200

    update example references

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-DIRS="CogniteSdk.Types Oryx.Cognite/src CogniteSdk/src CogniteSdk.FSharp/src CogniteSdk/test/fsharp CogniteSdk/test/csharp playground/csharp playground/fsharp"
+DIRS="CogniteSdk.Types Oryx.Cognite/src CogniteSdk/src CogniteSdk.FSharp/src CogniteSdk/test/fsharp CogniteSdk/test/csharp Examples/csharp Examples/fsharp"
 for dir in $DIRS; do
 	rm -rf $dir/bin $dir/obj
 done
