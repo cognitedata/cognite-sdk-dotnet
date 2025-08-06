7f4157bf8f4b350e21a1c1fdd7ea048763318868
abdullah-cognite <100700755+abdullah-cognite@users.noreply.github.com>
2025-08-06 09:55:39 +0200

    feat: add stream template settings (#439)
    * chore : remove templates api
    
    * add settings param to StreamWrite
    
    * define structure for stream settings
    
    * add stream name enums
    
    * fix formatting warnings
    
    * add json converter for name enum in streams
    
    * remove stream name template converter fs
    
    * format Converters.fs
    
    * remove redundant converter (fs)
    
    * enable streamRecords tests
    
    * enable stream deletion support
    
    * fix formatting
    
    * make the test more robust
    
    * formatting fixes
    
    * deserialize settings
    
    * remove settings check
    
    * update comments
    
    * modify cleanup
    
    * fix formatting
    
    * Review feedback
    
    Also changes the `type` field to be an enum as well. It's unlikely to
    change considerably.
    
    * Bump version
    
    * Fix stream type serialization
    
    ---------
    
    Co-authored-by: Einar Omang <einar.omang@cognite.com>
    Co-authored-by: Einar <einar@omang.com>

--- CogniteSdk.Types/Beta/StreamRecords/Stream.cs
+++ CogniteSdk.Types/Beta/StreamRecords/Stream.cs
@@ -1,5 +1,7 @@
-// Copyright 2024 Cognite AS
+// Copyright 2025 Cognite AS
 // SPDX-License-Identifier: Apache-2.0
+
+using System.Text.Json.Serialization;
 
 namespace CogniteSdk.Beta
 {
@@ -15,6 +17,86 @@ namespace CogniteSdk.Beta
         /// Stream id cannot be "logs" or "records". Max length is 100 characters.
         /// </summary>
         public string ExternalId { get; set; }
+
+        /// <summary>
+        /// Settings for the stream. Required field.
+        /// </summary>
+        public StreamSettings Settings { get; set; }
+    }
+
+    /// <summary>
+    /// Settings for a stream.
+    /// </summary>
+    public class StreamSettings
+    {
+        /// <summary>
+        /// Template settings for the stream.
+        /// </summary>
+        public StreamTemplateSettings Template { get; set; }
+    }
+
+    /// <summary>
+    /// The name of the stream template.
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum StreamTemplateName
+    {
+        /// <summary>
+        /// Immutable test stream template
+        /// </summary>
+        ImmutableTestStream,
+
+        /// <summary>
+        /// Immutable data staging template
+        /// </summary>
+        ImmutableDataStaging,
+
+        /// <summary>
+        /// Immutable normalized data template
+        /// </summary>
+        ImmutableNormalizedData,
+
+        /// <summary>
+        /// Immutable archive template
+        /// </summary>
+        ImmutableArchive,
+
+        /// <summary>
+        /// Mutable test stream template
+        /// </summary>
+        MutableTestStream,
+
+        /// <summary>
+        /// Mutable live data template
+        /// </summary>
+        MutableLiveData
+    }
+
+    /// <summary>
+    /// Template settings for a stream.
+    /// </summary>
+    public class StreamTemplateSettings
+    {
+        /// <summary>
+        /// The name of the stream template.
+        /// </summary>
+        public StreamTemplateName Name { get; set; }
+    }
+
+    /// <summary>
+    /// Type of stream, either Immutable or Mutable.
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum StreamType
+    {
+        /// <summary>
+        /// Immutable stream type.
+        /// </summary>
+        Immutable,
+        /// <summary>
+        /// Mutable stream type.
+        /// </summary>
+        Mutable
     }
 
     /// <summary>
@@ -26,5 +108,17 @@ namespace CogniteSdk.Beta
         /// Time the stream was created, in milliseconds since epoch.
         /// </summary>
         public long CreatedTime { get; set; }
+
+        /// <summary>
+        /// Name of the template used for creating this stream.
+        /// Only present when using alpha cdf-version header.
+        /// </summary>
+        public string CreatedFromTemplate { get; set; }
+
+        /// <summary>
+        /// Type of the stream (Immutable or Mutable).
+        /// Only present when using alpha cdf-version header.
+        /// </summary>
+        public StreamType? Type { get; set; }
     }
 }
